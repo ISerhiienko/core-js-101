a1a6e846fa89ad097a82dcafa6288d08b6f3e2ca
Ihor Serhiienko <igorserhiyenko@gmail.com>
2023-06-08 14:14:27 +0300

    done Factorial tasks

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -50,8 +50,18 @@ function getFizzBuzz(num) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  let factorial = 1;
+
+  for (let i = 2; i <= n; i += 1) {
+    factorial *= i;
+  }
+
+  return factorial;
 }
 
 
